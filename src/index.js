ad1203e92ffd92ebb844e2d77013bcd6ca2b3eab
Padma Patil <abdrazakovermek9@gmail.com>
2023-12-24 10:06:00 -0800

    It will add reset button to refresh DOM

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import AgeCalculator from './js/galactic.js';
 
-
 function handleForm(e) {
     e.preventDefault();
     const currentAge = document.getElementById("currentAge").value;
@@ -11,23 +10,23 @@ function handleForm(e) {
     const futureAge = document.getElementById("futureAge").value;
     const ageCalculator = new AgeCalculator(parseInt(currentAge));
 
-    document.getElementById("currentEarth").innerText = `Current age on Earth: ${ageCalculator.getAge(currentAge)}`;
-    document.getElementById("currentMercury").innerText = `Current age on Mercury: ${ageCalculator.mercuryAgeCalculator(currentAge)}`;
-    document.getElementById("currentVenus").innerText = `Current age on Venus: ${ageCalculator.venusAgeCalculator(currentAge)}`;
-    document.getElementById("currentMars").innerText = `Current age on Mars: ${ageCalculator.marsAgeCalculator(currentAge)}`;
-    document.getElementById("currentJupiter").innerText = `Current age on Jupiter: ${ageCalculator.jupiterAgeCalculator(currentAge)}`;
+    document.getElementById("currentEarth").innerText = `Earth: ${ageCalculator.getAge(currentAge)}`;
+    document.getElementById("currentMercury").innerText = `Mercury: ${ageCalculator.mercuryAgeCalculator(currentAge)}`;
+    document.getElementById("currentVenus").innerText = `Venus: ${ageCalculator.venusAgeCalculator(currentAge)}`;
+    document.getElementById("currentMars").innerText = `Mars: ${ageCalculator.marsAgeCalculator(currentAge)}`;
+    document.getElementById("currentJupiter").innerText = `Jupiter: ${ageCalculator.jupiterAgeCalculator(currentAge)}`;
 
-    document.getElementById("pastEarth").innerText = `Years have passed since the birthday on Earth: ${ageCalculator.pastYearEarth(pastAge)}`;
-    document.getElementById("pastMercury").innerText = `Years have passed since the birthday on Mercury: ${ageCalculator.pastYearMercury(pastAge)}`;
-    document.getElementById("pastVenus").innerText = `Years have passed since the birthday on Venus: ${ageCalculator.pastYearVenus(pastAge)}`;
-    document.getElementById("pastMars").innerText = `Years have passed since the birthday on Mars: ${ageCalculator.pastYearMars(pastAge)}`;
-    document.getElementById("pastJupiter").innerText = `Years have passed since the birthday on Jupiter: ${ageCalculator.pastYearJupiter(pastAge)}`;
+    document.getElementById("pastEarth").innerText = `Earth: ${ageCalculator.pastYearEarth(pastAge)}`;
+    document.getElementById("pastMercury").innerText = `Mercury: ${ageCalculator.pastYearMercury(pastAge)}`;
+    document.getElementById("pastVenus").innerText = `Venus: ${ageCalculator.pastYearVenus(pastAge)}`;
+    document.getElementById("pastMars").innerText = `Mars: ${ageCalculator.pastYearMars(pastAge)}`;
+    document.getElementById("pastJupiter").innerText = `Jupiter: ${ageCalculator.pastYearJupiter(pastAge)}`;
 
-    document.getElementById("futureEarth").innerText = `Years until your future birthday on Earth: ${ageCalculator.futureYearEarth(futureAge)}`;
-    document.getElementById("futureMercury").innerText = `Years until your future birthday on Mercury: ${ageCalculator.futureYearMercury(futureAge)}`;
-    document.getElementById("futureVenus").innerText = `Years until your future birthday on Venus: ${ageCalculator.futureYearVenus(futureAge)}`;
-    document.getElementById("futureMars").innerText = `Years until your future birthday on Mars: ${ageCalculator.futureYearMars(futureAge)}`;
-    document.getElementById("futureJupiter").innerText = `Years until your future birthday on Jupiter: ${ageCalculator.futureYearJupiter(futureAge)}`;
+    document.getElementById("futureEarth").innerText = `Earth: ${ageCalculator.futureYearEarth(futureAge)}`;
+    document.getElementById("futureMercury").innerText = `Mercury: ${ageCalculator.futureYearMercury(futureAge)}`;
+    document.getElementById("futureVenus").innerText = `Venus: ${ageCalculator.futureYearVenus(futureAge)}`;
+    document.getElementById("futureMars").innerText = `Mars: ${ageCalculator.futureYearMars(futureAge)}`;
+    document.getElementById("futureJupiter").innerText = `Jupiter: ${ageCalculator.futureYearJupiter(futureAge)}`;
 
     document.getElementById("currentDiv").removeAttribute("class", "hidden");
     document.getElementById("pastDiv").removeAttribute("class", "hidden");
@@ -36,3 +35,10 @@ function handleForm(e) {
 }
 
 document.getElementById("form").addEventListener("submit", handleForm);
+document.getElementById("resetButton").addEventListener("click", () => {
+    document.getElementById("form").reset();
+
+    document.getElementById("currentDiv").setAttribute("class", "hidden");
+    document.getElementById("pastDiv").setAttribute("class", "hidden");
+    document.getElementById("futureDiv").setAttribute("class", "hidden");
+});
